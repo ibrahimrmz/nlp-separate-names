c9c8ad39adee454b89e9c1378c77c1dbacc9741c
Ibrahim <leonardo.sosa@vianney.com.mx>
2023-12-19 12:06:50 -0600

    [feat] More data in the latam_names csv and refactored index.js

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 const fs = require('fs');
 const { parse } = require('csv-parse');
-const { NerManager, NlgManager, NlpManager } = require('node-nlp');
+const { NerManager } = require('node-nlp');
 
 // Train and save the model.
 (async () => {
-    const manager = new NerManager({ threshold: 0.5 });
-    //const manager = new NlpManager({ languages: ['es'] });
+    const manager = new NerManager({ threshold: 0.8 });
 
     // Load CSV data
-    const csvData = fs.readFileSync('latam_names.csv', 'utf8');
-    const records = parse(csvData, { columns: true });
+    try {
+        const csvData = fs.readFileSync('latam_names.csv', 'utf8');
+        const records = parse(csvData, { columns: true });
 
-    let names = [];
-    let middleNames = [];
-    let lastNames = [];
-    let secondLastNames = [];
+        let names = [];
+        let middleNames = [];
+        let lastNames = [];
+        let secondLastNames = [];
 
-    await records.forEach(record => {
-        const { FirstName, MiddleName, LastName, SecondLastName } = record;
-        names.push(FirstName);
-        middleNames.push(MiddleName);
-        lastNames.push(LastName);
-        secondLastNames.push(SecondLastName);
+        await records.forEach(record => {
+            const { FirstName, MiddleName, LastName, SecondLastName } = record;
+            names.push(FirstName);
+            middleNames.push(MiddleName);
+            lastNames.push(LastName);
+            secondLastNames.push(SecondLastName);
+    
+        });
 
-    });
-    console.log("N. Names: " + names.length);
-    // Agregar entidades personalizadas
-    await manager.addNamedEntityText('name', 'name', ['es'], names);
-    await manager.addNamedEntityText('midlleName', 'midlleName', ['es'], middleNames);
-    await manager.addNamedEntityText('lastName', 'lastName', ['es'], lastNames);
-    await manager.addNamedEntityText('secondLastName', 'secondLastName', ['es'], secondLastNames);
+        console.log("N. Names: " + names.length);
 
-    /* await manager.train();
-    await manager.save(); */
-    const entities = manager.findEntities('PAULINA DE LA ROSA HERNANDEZ', 'es');
-    console.log(entities)
-    /* const response = await manager.process('MARIA DE LOURDES RAMIREZ HERNANDEZ');
-    console.log(response); */
+        // Agregar entidades personalizadas
+        await manager.addNamedEntityText('name', 'name', ['es'], names);
+        await manager.addNamedEntityText('middleName', 'middleName', ['es'], middleNames);
+        await manager.addNamedEntityText('lastName', 'lastName', ['es'], lastNames);
+        await manager.addNamedEntityText('secondLastName', 'secondLastName', ['es'], secondLastNames);
+
+        const entities = await manager.findEntities('MARIA DE ROSA ESCUTIA', 'es');
+        console.log(entities);
+    } catch (error) {
+        console.error('Error:', error);
+    }
 })();
-
